ac11ec83cf8b27f20a29a01eb54b47df41a53de6
omorikitty <omocatsun@gmail.com>
2023-12-20 16:33:37 -0500

    welcome api8

---
+++ dist/ba_data/python/bascenev1/_appmode.py
@@ -0,0 +1,42 @@
+# Released under the MIT License. See LICENSE for details.
+#
+"""Provides AppMode functionality."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from bacommon.app import AppExperience
+from babase import AppMode, AppIntentExec, AppIntentDefault
+
+import _bascenev1
+
+if TYPE_CHECKING:
+    from babase import AppIntent
+
+
+class SceneV1AppMode(AppMode):
+    """Our app-mode."""
+
+    @classmethod
+    def get_app_experience(cls) -> AppExperience:
+        return AppExperience.MELEE
+
+    @classmethod
+    def _supports_intent(cls, intent: AppIntent) -> bool:
+        # We support default and exec intents currently.
+        return isinstance(intent, AppIntentExec | AppIntentDefault)
+
+    def handle_intent(self, intent: AppIntent) -> None:
+        if isinstance(intent, AppIntentExec):
+            _bascenev1.handle_app_intent_exec(intent.code)
+            return
+        assert isinstance(intent, AppIntentDefault)
+        _bascenev1.handle_app_intent_default()
+
+    def on_activate(self) -> None:
+        # Let the native layer do its thing.
+        _bascenev1.on_app_mode_activate()
+
+    def on_deactivate(self) -> None:
+        # Let the native layer do its thing.
+        _bascenev1.on_app_mode_deactivate()
