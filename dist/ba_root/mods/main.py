0d0865c955688e6ab7ee7b41f7c07b63bf321d49
omorikitty <omocatsun@gmail.com>
2024-01-11 19:59:21 -0500

    Update main.py

--- dist/ba_root/mods/main.py
+++ dist/ba_root/mods/main.py
@@ -2,48 +2,48 @@
 
 import sys
 import os
+import bascenev1 as bs
 import importlib
 import _babase
 import babase
+import bascenev1._hooks
 from myspaz import spazmod
+
 
 # ba_meta export babase.Plugin
 
+
 class setup(babase.Plugin):
     def on_app_running(self):
-        #print('HELLO!')
+        # print('HELLO!')
         runMods()
 
-
     def on_app_shutdown(self):
-        print('Goodbye!')
-
-
+        print("Goodbye!")
 
 
 def runMods():
     """
-    esta funcion almacena todas nuestras modificaciones 
+    esta funcion almacena todas nuestras modificaciones
     """
     import_gamemodes()
     spazmod.run()
 
+
 def import_gamemodes():
-    """ Usaremos esta funcion para importar los modos de juegos de manera dinamica """
-    sys.path.append(_babase.env()['python_directory_user'] + os.sep + "gamemodes")
+    """Usaremos esta funcion para importar los modos de juegos de manera dinamica"""
+    sys.path.append(_babase.env()["python_directory_user"] + os.sep + "gamemodes")
     gamemodes = os.listdir("ba_root/mods/gamemodes")
     for gamemode in gamemodes:
         if gamemode.endswith(".py"):
             gamemode_name = gamemode.replace(".py", "")
             module_name = "gamemodes." + gamemode_name
-            #print(f"Intentando importar: {module_name}")
+            # print(f"Intentando importar: {module_name}")
             try:
                 # importamos los modulos
-                #print('Modos de jugo Importados Exitosamente!')
+                # print('Modos de jugo Importados Exitosamente!')
                 __import__(module_name)
             except ImportError as e:
                 print(f"Fallo al importar {module_name}: {e}")
 
-    #print('Hello gamemodes!')
-
-
+    # print('Hello gamemodes!')
