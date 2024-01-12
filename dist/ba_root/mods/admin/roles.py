5f3f47319ba6f455bba3e4ca6a15ee29eeeb5eb3
omorikitty <omocatsun@gmail.com>
2024-01-11 19:50:26 -0500

    update roles.py

--- dist/ba_root/mods/admin/roles.py
+++ dist/ba_root/mods/admin/roles.py
@@ -3,8 +3,8 @@ import babase
 import json
 import os
 
-ROLES_PATH = os.path.join(babase.env().get("python_directory_user", ""), "admin/admin.json")
 
+ROLES_PATH = os.path.join(babase.env().get("python_directory_user", ""), "admin.json")
 
 
 def make_a_dir(name):
@@ -12,42 +12,40 @@ def make_a_dir(name):
     if not os.path.exists(name):
         os.mkdir(name)
 
+
 def dontAdmin(account):
     """Esta funcion basicamente comprueba si el account_id
-       no se encuentra dentro de los roles
+    no se encuentra dentro de los roles
 
-       retorna True si es que no se encuentra dentro
-       de la lista 
+    retorna True si es que no se encuentra dentro
+    de la listas de id que pertenecen a cada rol
     """
     rol = get_all_roles()
     if rol is not None:
         for i in rol.values():
             if account in i["id"]:
                 return False
-                
-
     return True
-            
+
 
 def whatRol(account):
-    """retorna el tipo de rol espesifico 
-    """
+    """retorna el tipo de rol espesifico"""
     rol = get_all_roles()
     if rol is not None:
         for i, k in rol.items():
             if account in k["id"]:
                 return i
-
+    return
 
 
 def get_all_roles():
-    """Esta funcion nos retorna todos los roles que existen
-    """
+    """Esta funcion nos retorna todos los roles que existen"""
     try:
-        with open(ROLES_PATH, mode="r",  encoding="utf-8") as rol:
+        with open(ROLES_PATH, mode="r", encoding="utf-8") as rol:
             return json.load(rol)
-    except:
+    except FileNotFoundError:
         return None
+
 
 def save(data: dict):
     """Guarda los cambios que hagamos en el admin.json"""
