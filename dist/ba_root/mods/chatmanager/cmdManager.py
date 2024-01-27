5864c2670509592b8ad0a488ab19db57248b133a
omorikitty <omocatsun@gmail.com>
2024-01-27 18:12:39 -0500

    Update cmdManager

--- dist/ba_root/mods/chatmanager/cmdManager.py
+++ dist/ba_root/mods/chatmanager/cmdManager.py
@@ -8,7 +8,7 @@ class CommandManager:
     def __init__(
         self,
         max_commands_per_minute: int = 5,
-        cooldown_duration: int = 5,
+        cooldown_duration: int = 10,
         admin_bypass_threshold: int = 5,
     ):
         """Inicializa el CommandManager.
@@ -18,7 +18,7 @@ class CommandManager:
             cooldown_duration (int): duracion de comandos por minutos. Defaults to 5.
             admin_bypass_threshold (int): Maximos comandos por admins. Defaults to 3.
         """
-        self.command_count = {}
+        #self.command_count = {}
         self.max_commands_per_minute = max_commands_per_minute
         self.command_cooldowns = {}
         self.cooldown_duration = cooldown_duration
@@ -42,15 +42,15 @@ class CommandManager:
         if command not in self.command_cooldowns:
             self.command_cooldowns[command] = {}
 
-        # Verifica si el comando esta en un periodo de espera
+
         if (
-            client_id in self.command_cooldowns.get(command, {})
-            and current_time - self.command_cooldowns[command][client_id]
+            client_id in self.command_cooldowns[command]
+            and current_time - self.command_cooldowns[command][client_id]["last"]
             < self.cooldown_duration
         ):
             remaining = int(
                 self.cooldown_duration
-                - (current_time - self.command_cooldowns[command][client_id])
+                - (current_time - self.command_cooldowns[command][client_id]["last"])
             )
             sendmsg(
                 f"Demasiadas Solicitudes Espera {remaining}s.",
@@ -58,28 +58,28 @@ class CommandManager:
                 color=(0.8, 0.7, 0.5),
             )
             # reinicia el contador para que pueda seguir usando el comando.
-            self.command_count[client_id] = 1
+            self.command_cooldowns[command][client_id]["count"] = 1
             return False
 
         # Actualiza el recuento de comandos para el usuario
-        if client_id in self.command_count:
-            self.command_count[client_id] += 1
+        if client_id in self.command_cooldowns[command]:
+            self.command_cooldowns[command][client_id]["count"] += 1
         else:
-            self.command_count[client_id] = 1
+            self.command_cooldowns[command][client_id] = {"count": 1, "last": 0}
 
         # Verifica si el usuario ha superado el limite maximo de comandos por minuto
-        if self.command_count[client_id] > self.max_commands_per_minute:
+        if self.command_cooldowns[command][client_id]["count"] > self.max_commands_per_minute:
             # Si el usuario supera el umbral, aplica un periodo de espera mas largo
-            self.command_cooldowns[command][client_id] = (
+            self.command_cooldowns[command][client_id]["last"] = (
                 current_time + self.cooldown_duration
             )
             return False
 
         # Bypass para administradores
         if account_id is not None and is_admin(account_id):
-            if self.command_count[client_id] > self.admin_bypass_threshold:
+            if self.command_cooldowns[command][client_id]["count"] > self.admin_bypass_threshold:
                 # Si un administrador supera el umbral, reduce el periodo de espera
-                self.command_cooldowns[command][client_id] = (
+                self.command_cooldowns[command][client_id]["last"] = (
                     current_time + self.cooldown_duration / 2
                 )
 
